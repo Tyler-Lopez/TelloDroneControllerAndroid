1efcd7e6fa948af501ac49ac80a1b4ce5fb42ea7
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-10-02 13:24:28 -0400

    Initial commit

---
+++ app/src/main/java/com/dodo/flashcards/architecture/BaseRoutingViewModel.kt
@@ -0,0 +1,18 @@
+package com.dodo.flashcards.architecture
+
+abstract class BaseRoutingViewModel<
+        TypeOfViewState : ViewState,
+        TypeOfViewEvent : ViewEvent,
+        TypeOfDestination : Destination>
+    : BaseViewModel<TypeOfViewState, TypeOfViewEvent>() {
+
+    private var router: Router<TypeOfDestination>? = null
+
+    fun attachRouter(router: Router<TypeOfDestination>) {
+        this.router = router
+        onRouterAttached()
+    }
+
+    /** Optional in implementing ViewModel **/
+    open fun onRouterAttached() {}
+}
