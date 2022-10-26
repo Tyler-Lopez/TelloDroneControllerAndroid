8f51c29ca6c4aa83be4bd4e916ae39191e50076d
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-10-25 20:28:09 -0400

    Work towards amplify auth impl

--- app/src/main/java/com/tlopez/tello_controller/domain/services/AuthClient.kt
+++ app/src/main/java/com/tlopez/tello_controller/domain/services/AuthClient.kt
@@ -1,12 +1,13 @@
 package com.tlopez.tello_controller.domain.services
 
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes
+import com.amplifyframework.auth.AuthSession
 
 interface AuthClient {
-    fun init()
     suspend fun signIn(username: String, password: String): Result<Unit>
     suspend fun updatePassword(oldPassword: String, newPassword: String): Result<Unit>
     suspend fun getAccessToken(): Result<String>
+    suspend fun fetchAuthSession(): Result<AuthSession>
     suspend fun fetchUserAttributes(): Result<CognitoUserAttributes>
     suspend fun logout(): Result<Unit>
 }
